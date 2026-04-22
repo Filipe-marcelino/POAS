6ff63605ab16dec4ea1fb95a3383f366c26e1ac4
Filipin <160949589+Filipe-marcelino@users.noreply.github.com>
2026-04-22 08:28:29 -0300

    Add files via upload

---
+++ api_banco/app.py
@@ -0,0 +1,6 @@
+from model import Tarefa
+from database import create_db, get_session
+from fastapi import FastAPI, Depends
+from typing import Annotated
+from sqlmodel import Session
+SessionDep = Annotated[Session,Depends(get_session)]
